b26e688e66997a344b98635e2921e9440665b381
Fidjiro <florian.lecointe621@gmail.com>
2018-05-31 15:51:39 +0200

    Ajout du convertisseur wgs vers lambert

--- MyApplication/MyApplication/app/src/main/java/com/example/florian/myapplication/Activities/LoginActivity.java
+++ MyApplication/MyApplication/app/src/main/java/com/example/florian/myapplication/Activities/LoginActivity.java
@@ -28,6 +28,9 @@ import com.example.florian.myapplication.R;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import static java.lang.Math.PI;
+import static java.lang.Math.log;
+
 /**
  * Activité login qui permet de se log via un login/mot de passe.
  */
@@ -52,6 +55,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        tryConvertL93ToWgs84();
+
         dao = new TaxUsrDAO(this);
         dao.open();
         loadData();
@@ -75,6 +80,32 @@ public class LoginActivity extends AppCompatActivity {
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    private double atanh(double x){
+        return (log(1+x) - log(1-x))/2;
+    }
+
+    private void tryConvertL93ToWgs84(){
+        // récupération des coordonnées
+        double latitude= 50.66760;
+        double longitude= 1.83971;
+
+// définition des constantes
+        double c= 11754255.426096; //constante de la projection
+        double e= 0.0818191910428158; //première exentricité de l'ellipsoïde
+        double n= 0.725607765053267; //exposant de la projection
+        double xs= 700000; //coordonnées en projection du pole
+        double ys= 12655612.049876; //coordonnées en projection du pole
+
+// pré-calculs
+        double lat_rad= latitude/180*Math.PI; //latitude en rad
+        double lat_iso= atanh(Math.sin(lat_rad))-e*atanh(e*Math.sin(lat_rad)); //latitude isométrique
+
+//calcul
+        double x= ((c*Math.exp(-n*(lat_iso)))*Math.sin(n*(longitude-3)/180*Math.PI)+xs);
+        double y= (ys-(c*Math.exp(-n*(lat_iso)))*Math.cos(n*(longitude-3)/180*Math.PI));
+        System.out.println("Latitude lambert : " + x + ", longitude lambert : " + y);
     }
 
     /**
