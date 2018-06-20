8a8bfd5a2841165280e1318501e637a5c39198d0
Fidjiro <florian.lecointe621@gmail.com>
2018-06-20 16:15:19 +0200

    début de la pop up pour rechercher un taxon

---
+++ MyApplication/MyApplication/app/src/main/java/com/example/florian/myapplication/Activities/MapsActivities/Recensement/SearchTaxonPopup.java
@@ -0,0 +1,15 @@
+package com.example.florian.myapplication.Activities.MapsActivities.Recensement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.florian.myapplication.R;
+
+public class SearchTaxonPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_taxon_popup);
+    }
+}
